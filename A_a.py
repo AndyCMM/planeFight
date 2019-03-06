91f5444d123ce348ab361ffc0c66163b5794ae8a
AndyCMM <953087547@qq.com>
2019-03-06 23:23:58 +0800

    梵蒂冈

---
+++ A_a.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+# @Time  :2019-03-05 23:48
+# @Author:AndyMing
+# @File  :A_a.py
+
+
+class A:
+    def __init__(self):
+        self.n = 2
+
+    def add(self, m):
+        print('self is {0} @A.add'.format(self))
+        self.n += m
+
+
+class B(A):
+    def __init__(self):
+        self.n = 3
+
+    def add(self, m):
+        print('self is {0} @B.add'.format(self))
+        super().add(m)
+        self.n += 3
+
+
+class C(A):
+    def __init__(self):
+        self.n = 4
+
+    def add(self, m):
+        print('self is {0} @C.add'.format(self), id(self))
+        super().add(m)
+        self.n += 4
+
+
+class D(B, C):
+    def __init__(self):
+        self.n = 5
+
+    def add(self, m):
+        print('self is {0} @D.add'.format(self), id(self))
+        super().add(m)
+        self.n += 5
+
+
+D().add(3)
