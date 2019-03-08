770029218707e5159d56be598a0fa14329eefe6f
AndyCMM <953087547@qq.com>
2019-03-08 21:07:33 +0800

    梵蒂冈

--- A_a.py
+++ A_a.py
@@ -19,7 +19,7 @@ class B(A):
 
     def add(self, m):
         print('self is {0} @B.add'.format(self))
-        super().add(m)
+        A.add(self, m)
         self.n += 3
 
 
@@ -34,13 +34,43 @@ class C(A):
 
 
 class D(B, C):
+    def __new__(cls, *args, **kwargs):
+        print('I am new')
+        return B.__new__(cls)
+
     def __init__(self):
+        super().__init__()
         self.n = 5
 
     def add(self, m):
         print('self is {0} @D.add'.format(self), id(self))
-        super().add(m)
+        B.add(self, m)
         self.n += 5
 
 
-D().add(3)
+# D().add(3)
+# print(D)
+# print(D().__dict__)
+
+class C:
+    pass
+
+
+def singlepe():
+    s = []
+
+    def single():
+        if len(s) == 0:
+            s.append(C())
+            return s[0]
+        else:
+            return s[0]
+    return single
+
+
+a = singlepe()
+b = a()
+print(id(b))
+c = a()
+print(id(c))
+
